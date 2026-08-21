a7b775ddfb0b7c9ac4523d54630cd80eeef306b7
Doruk Kök <dorukkok513@gmail.com>
2026-08-21 14:27:28 +0300

    Spring boot projesi olusturuldu

---
+++ src/main/java/com/example/butcetakipapi/ButceTakipApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.butcetakipapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButceTakipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButceTakipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/butcetakipapi/ButceTakipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.butcetakipapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButceTakipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
